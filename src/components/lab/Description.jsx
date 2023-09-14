9924d06628a67f5d4b8964069f9279c7d3d675c7
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-09-14 20:15:09 +0300

    update coagulation

--- src/components/lab/Description.jsx
+++ src/components/lab/Description.jsx
@@ -1,15 +1,27 @@
 import PropType from 'prop-types';
 
 const Description = ({ equipment }) => {
-  const { description } = equipment;
+  const { description, features } = equipment;
 
   const renderDescription = (description) => {
     return (
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4">
         {description.map((item, index) => (
           <ul className="text-sm flex gap-10 list-disc" key={index}>
             <li>{item}</li>
           </ul>
+        ))}
+      </div>
+    );
+  };
+
+  const renderFeatures = (features) => {
+    return (
+      <div className="flex flex-col gap-6">
+        {features.map((item, index) => (
+          <div className="text-sm flex flex-col gap-6" key={index}>
+            <p>§ {item}</p>
+          </div>
         ))}
       </div>
     );
@@ -23,6 +35,15 @@ const Description = ({ equipment }) => {
           <div className="mt-4 flex flex-col gap-6">
             {renderDescription(description)}
           </div>
+
+          {features && features.length > 0 && (
+            <>
+              <h2 className="text-md font-semibold mt-8">Advanced Features</h2>
+              <div className="mt-4 flex flex-col gap-10">
+                {renderFeatures(features)}
+              </div>
+            </>
+          )}
 
           <button className="bg-primaryGreen text-white rounded-lg px-4 py-2 mt-8">
             Download File
