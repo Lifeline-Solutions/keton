531b2e450c0a579a28cdd8d01ae5699643b560b4
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-09-16 13:56:02 +0300

    feat: replace products on home page

--- src/components/home-page/Products.jsx
+++ src/components/home-page/Products.jsx
@@ -3,24 +3,26 @@ import {
   BiSolidChevronRightCircle,
   BiSolidChevronLeftCircle,
 } from 'react-icons/bi';
+import { equipmentData } from '../../data/equipmentData';
 
-const products = [
-  {
-    id: 1,
-    image: '/images/medicine-bottles.png',
-    name: 'Control FD Immunoassay',
-  },
-  {
-    id: 2,
-    image: '/images/cooler.png',
-    name: 'Cooler box with inbuilt digital Thermometer',
-  },
-  {
-    id: 3,
-    image: '/images/medicine-bottles-2.png',
-    name: 'Control FD Glycohemoglobin A1c',
-  },
-];
+//   {
+//     id: 1,
+//     image: '/images/medicine-bottles.png',
+//     name: 'Control FD Immunoassay',
+//   },
+//   {
+//     id: 2,
+//     image: '/images/cooler.png',
+//     name: 'Cooler box with inbuilt digital Thermometer',
+//   },
+//   {
+//     id: 3,
+//     image: '/images/medicine-bottles-2.png',
+//     name: 'Control FD Glycohemoglobin A1c',
+//   },
+// ];
+
+const products = equipmentData.map((category) => category.products).flat();
 
 const Products = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -30,7 +32,7 @@ const Products = () => {
     const interval = setInterval(() => {
       const nextIndex = (currentIndex + itemsPerPage) % products.length;
       setCurrentIndex(nextIndex);
-    }, 3000);
+    }, 8000);
 
     return () => clearInterval(interval);
   }, [currentIndex]);
@@ -75,13 +77,13 @@ const Products = () => {
           >
             <img
               src={product.image}
-              alt={product.name}
+              alt={product.title}
               className="sm:w-56 sm:h-56 w-24 h-24 mx-auto mb-4"
             />
-            <h3 className="text-center sm:text-base text-sm">{product.name}</h3>
-            <button className="block mx-auto mt-4 bg-primaryGreen text-white px-4 py-2 rounded sm:text-sm text-xs">
+            <h3 className="text-center sm:text-base text-sm">{product.title}</h3>
+            <a href={`lab-equipment-and-supplies/${product.id}`} className="block mx-auto mt-4 bg-primaryGreen text-white px-4 py-2 rounded sm:text-sm text-xs text-center">
               Read More
-            </button>
+            </a>
           </div>
         ))}
         <button onClick={handleNext} className="sm:ml-2 ml-0">
