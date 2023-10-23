04bdc00afbdcc6d24375d43f7c06bf05ad82fe2c
Annastacia Mumbua Kioko <anne@mradi.co.ke>
2023-10-23 09:34:07 +0300

    microcontrol popup

--- src/components/third-party/Clinical.jsx
+++ src/components/third-party/Clinical.jsx
@@ -1,67 +1,25 @@
-const microcontrols = [
-  {
-    id: 1,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/equip-29_upluj7.png',
-    title: 'Cal Ver LQ Blood Gas',
-  },
-  {
-    id: 2,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_231_busfhu.png',
-    title: 'Control Drop LQ Blood Glucose',
-  },
-  {
-    id: 3,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_232_qvxi2l.png',
-    title: 'Control Drop LQ Blood Glucose',
-  },
-  {
-    id: 4,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_233_ia17hm.png',
-    title: 'Control Drop LQ Urine Dipstick, 25ml',
-  },
-  {
-    id: 5,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_238_fcaijb.png',
-    title: 'Control FD Assayed Chemistry, Level 2',
-  },
-  {
-    id: 6,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_239_wz8smp.png',
-    title: 'Control  FD BNP',
-  },
-  {
-    id: 7,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_234_wg4vgu.png',
-    title: 'Control FD Cardiac Markers',
-  },
-  {
-    id: 8,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_235_u7z5hy.png',
-    title: 'Control FD Glycohemoglobin A1c',
-  },
-  {
-    id: 9,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_236_pyckkg.png',
-    title: 'Control FD Immunoassay',
-  },
-  {
-    id: 10,
-    image:
-      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899386/keton/Rectangle_237_i19akm.png',
-    title: 'Control LQ Homocysteine',
-  },
-];
+import microcontrols from '../../data/microcontrols';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const Clinical = () => {
+  const contentStyle = {
+    maxWidth: '85%',
+    width: '85%',
+    maxHeight: '85%',
+    overflow: 'auto',
+    margin: 'auto',
+    background: 'rgba(0,0,0,0)',
+    border: 'none',
+    borderRadius: 'none',
+    padding: '0',
+  };
+
+  const overlayStyle = {
+    background: 'rgba(0,0,0,0.5)',
+    backdropFilter: 'blur(5px)',
+  };
+
   return (
     <div className="flex flex-col justify-center gap-4 p-6 bg-primaryBg">
       <>
@@ -87,9 +45,76 @@ const Clinical = () => {
               />
               <div className="flex flex-col justify-center items-center">
                 <p className="text-sm  mt-2">{microcontrol.title}</p>
-                <button className="bg-primaryGreen text-white text-sm px-4 py-2 rounded-lg mt-2">
-                  Read More
-                </button>
+                <Popup
+                  key={microcontrol.id}
+                  trigger={
+                    <button className="bg-primaryGreen text-white text-sm px-4 py-2 rounded-lg mt-2">
+                      Read More
+                    </button>
+                  }
+                  modal
+                  nested
+                  position="center center"
+                  closeOnDocumentClick
+                  contentStyle={contentStyle}
+                  overlayStyle={overlayStyle}
+                >
+                  <div className="flex flex-col gap-2 bg-white">
+                    <div className="flex justify-end mr-8">
+                      <button className="text-3xl outline-none" onClick={close}>
+                        &times;
+                      </button>
+                    </div>
+                    <div className="grid sm:grid-cols-2 grid-cols-1">
+                      <div className="col-span-1 px-4">
+                        <img
+                          src={microcontrol.image}
+                          alt="logo"
+                          className="w-3/4 h-3/4 rounded-lg"
+                          loading="lazy"
+                        />
+                      </div>
+                      <div className="col-span-1 flex flex-col gap-2 text-sm mt-4 w-3/4 leading-6 px-4">
+                        <h1 className="text-xl capitalize mb-4 font-bold">
+                          {microcontrol.title}
+                        </h1>
+                        {microcontrol.description && microcontrol.description.length > 0 && (
+                        <>
+                        {microcontrol.description}
+                        </>
+                        )}
+                      </div>
+                    </div>
+                    <div className='px-8 py-4 text-sm leading-6'>
+                    {microcontrol.features && microcontrol.features.length > 0 && (
+                        <div className='py-4'>
+                        <h4 className="text-md capitalize mb-4 font-bold">
+                          Features
+                        </h4>
+                        {
+                          microcontrol.features.map((feature) => (
+                            <p key={feature}>{feature}</p>
+                          ))
+                        }
+                        </div>
+                        )}
+                    {microcontrol.analytes && microcontrol.analytes.length > 0 && (
+                        <>
+                        <h4 className="text-md capitalize mb-4 font-bold">
+                          analytes
+                        </h4>
+                        <ul className='list-disc list-inside'>
+                        {
+                          microcontrol.analytes.map((feature) => (
+                            <li key={feature}>{feature}</li>
+                          ))
+                        }
+                        </ul>
+                        </>
+                        )}
+                    </div>
+                  </div>
+                </Popup>
               </div>
             </div>
           ))}
---
+++ src/data/microcontrols.js
@@ -0,0 +1,83 @@
+const microcontrols = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/equip-29_upluj7.png',
+    title: 'Cal Ver LQ Blood Gas',
+    description: "Blood Gas Calibration Verification is assayed quality control material consisting of five levels of buffers and salts in a bovine based material and equilibrated with known levels of Carbon Dioxide (CO2), Oxygen (O2), and Nitrogen.Cal Ver LQ Blood Gas. Blood Gas Calibration Verification is assayed quality control material consisting of five levels of buffers and salts in a bovine based material and equilibrated with known levels of Carbon Dioxide (CO2), Oxygen (O2), and Nitrogen.",
+    features: [
+      'A quality control material intended for use in the quantitative verification of calibration and reportable range.',
+      'Storage',
+      '60 seconds when stored at 2-8°C.',
+    ],
+    analytes: [
+      'BUN',
+      'Chloride',
+      'Creatinine',
+      'Glucose',
+      'Ionized Calcium',
+      'Lactic Acid',
+      'pCO2',
+      'pH',
+      'pO2',
+      'Potassium',
+      'Sodium',
+    ],
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_231_busfhu.png',
+    title: 'Control Drop LQ Blood Glucose',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_232_qvxi2l.png',
+    title: 'Control Drop LQ Blood Glucose',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899388/keton/Rectangle_233_ia17hm.png',
+    title: 'Control Drop LQ Urine Dipstick, 25ml',
+  },
+  {
+    id: 5,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_238_fcaijb.png',
+    title: 'Control FD Assayed Chemistry, Level 2',
+  },
+  {
+    id: 6,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_239_wz8smp.png',
+    title: 'Control  FD BNP',
+  },
+  {
+    id: 7,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_234_wg4vgu.png',
+    title: 'Control FD Cardiac Markers',
+  },
+  {
+    id: 8,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_235_u7z5hy.png',
+    title: 'Control FD Glycohemoglobin A1c',
+  },
+  {
+    id: 9,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899387/keton/Rectangle_236_pyckkg.png',
+    title: 'Control FD Immunoassay',
+  },
+  {
+    id: 10,
+    image:
+      'https://res.cloudinary.com/dlahz5ciz/image/upload/v1697899386/keton/Rectangle_237_i19akm.png',
+    title: 'Control LQ Homocysteine',
+  },
+];
+
+export default microcontrols;
